3c0a32778c7c623340d46d9a3751edb45fc6e024
Hector Haro H <hector8.haro@gmail.com>
2022-05-11 23:41:29 -0500

    Add React Router DOM and new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React, { Fragment} from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./components/Header";
+import Products from "./components/Products";
+import NuevoProducto from "./components/NuevoProducto";
+import EditarProducto from "./components/EditarProducto";
 
 function App() {
   return (
-    <fragment>
+    <Router>
       <Header />
-    </fragment>
+        <div className="container" mt-5>
+          <Routes >
+            <Route path="/" element={<Products/>}/>
+            <Route path="/productos/nuevo" element={<NuevoProducto/>}/>
+            <Route path="/productos/editar/:id" element={<EditarProducto/>}/>
+          </Routes >
+        </div>
+    </Router>
   );
 }
 
