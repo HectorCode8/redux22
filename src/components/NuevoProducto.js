0ec2eef65197ce278e86a0da85a205e79f3e133f
Hector Haro H <hector8.haro@gmail.com>
2022-05-12 13:54:21 -0500

    Define actions to use in the dispatch

--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -1,6 +1,26 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+// Actions de Redux
+import { crearNuevoProductoAction } from '../Actions/productoActions'
+
 
 const NuevoProducto = () => {
+
+    const dispatch = useDispatch();
+
+    const agregarProducto = () => dispatch(crearNuevoProductoAction())
+
+    const submitNuevoProducto = (e) => {
+        e.preventDefault();
+
+
+
+
+        agregarProducto()
+    }
+
+
   return (
     <div className="row justify-content-center">
         <div className="col-md-8">
@@ -9,7 +29,9 @@ const NuevoProducto = () => {
                     <h2 className="text-center mb-4 font-weight-bold">
                         Agregar Nuevo Producto
                     </h2>
-                    <form>
+                    <form
+                        onSubmit={submitNuevoProducto}
+                    >
                         <div className="form-group">
                             <label>Nombre del Producto</label>
                             <input
